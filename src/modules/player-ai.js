c75d7836ba569e51c827b1b6d3dda53c92e13914
Chris <karampagiaschris@gmail.com>
2023-07-12 16:43:38 +0300

    Delete player factory, modify makeRandomChoice

--- src/modules/player-ai.js
+++ src/modules/player-ai.js
@@ -1,30 +1,14 @@
-const Player = (name) => {
-  let turn = false;
-
-  const isTurn = () => turn;
-
-  const changeTurnStatus = () => {
-    if (turn) {
-      turn = false;
-    } else {
-      turn = true;
-    }
-  };
-
-  return { name, isTurn, changeTurnStatus };
-};
-
 const Computer = () => {
   const makeRandomChoice = (board) => {
     let i = Math.floor(Math.random() * 10),
       j = Math.floor(Math.random() * 10);
-    while (board[i][j][3]) {
+    while (board.showBoard()[i][j][3]) {
       i = Math.floor(Math.random() * 10);
       j = Math.floor(Math.random() * 10);
     }
-    board[i][j][3] = true;
+    return [i, j];
   };
   return { makeRandomChoice };
 };
 
-export { Player, Computer };
+export { Computer };
