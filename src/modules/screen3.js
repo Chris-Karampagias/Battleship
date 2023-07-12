51665e650a18851db1ee3fad07c20bd657927b8e
Chris <karampagiaschris@gmail.com>
2023-07-12 16:46:14 +0300

    Code screen3Logic

--- src/modules/screen3.js
+++ src/modules/screen3.js
@@ -1,6 +1,7 @@
 import { highlightCells } from "./screen2";
+import { Computer } from "./player-ai";
 
-function createScreen3(playerBoard) {
+function createScreen3(playerBoard, name) {
   const body = document.querySelector("body");
   const screen3 = document.createElement("div");
   screen3.className = "screen3";
@@ -8,7 +9,7 @@ function createScreen3(playerBoard) {
   messageContainer.className = "message-container";
   const message = document.createElement("div");
   message.className = "message-screen3";
-  message.textContent = "Hello";
+  message.textContent = `Awaiting for orders ${name}`;
   messageContainer.append(message);
   const boards = document.createElement("div");
   boards.className = "boards";
@@ -63,4 +64,74 @@ function createAiGrid() {
   return grid;
 }
 
-export { createScreen3 };
+function screen3Logic(playerBoard, aiBoard, name) {
+  const ai = Computer();
+  const message = document.querySelector(".message-screen3");
+  const playerCells = Array.from(document.querySelectorAll(".player-cell"));
+  const aiCells = Array.from(document.querySelectorAll(".ai-cell"));
+  aiCells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      const coords = [
+        Number(e.target.attributes[2].value),
+        Number(e.target.attributes[1].value),
+      ];
+      aiCells.forEach((cell) => {
+        cell.classList.add("no-pointer-events");
+      });
+      if (aiBoard.receiveAttack(coords)) {
+        e.target.classList.add("hit");
+        e.target.classList.add("no-pointer-events");
+        setTimeout(() => {
+          message.textContent = "It's a hit!";
+        }, 200);
+        if (aiBoard.allShipsSunk()) {
+          setTimeout(() => {
+            message.textContent = `You win ${name}!`;
+          }, 700);
+          return;
+        }
+      } else {
+        e.target.classList.add("miss");
+        e.target.classList.add("no-pointer-events");
+        setTimeout(() => {
+          message.textContent = "It's a miss!";
+        }, 200);
+      }
+      setTimeout(() => {
+        message.textContent = "It's enemy's turn!";
+        setTimeout(() => {
+          const coords = ai.makeRandomChoice(playerBoard);
+          if (playerBoard.receiveAttack(coords)) {
+            setTimeout(() => {
+              message.textContent = "It's a hit!";
+            }, 200);
+            if (playerBoard.allShipsSunk()) {
+              setTimeout(() => {
+                message.textContent = "You lose!";
+              }, 1000);
+              return;
+            }
+          } else {
+            setTimeout(() => {
+              message.textContent = "It's a miss!";
+            }, 200);
+          }
+          playerBoard.markHitCells(playerCells);
+          setTimeout(() => {
+            message.textContent = "It's your turn!";
+            aiCells.forEach((cell) => {
+              if (
+                !cell.classList.contains("hit") &&
+                !cell.classList.contains("miss")
+              ) {
+                cell.classList.remove("no-pointer-events");
+              }
+            });
+          }, 1300);
+        }, 1300);
+      }, 1500);
+    });
+  });
+}
+
+export { screen3Logic, createScreen3 };
