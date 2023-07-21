174f3233889cf9f868fa171e72a88ec55a98bc32
Chris <karampagiaschris@gmail.com>
2023-07-21 12:36:07 +0300

    Revert changes for screen1 code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,28 @@
-/* eslint-disable no-unused-vars */
 import "./styles/screen1.css";
 import "./styles/screen2.css";
 import "./styles/screen3.css";
-import { Player, Computer } from "./modules/player-ai";
-import { Gameboard } from "./modules/gameboard";
-import { createScreen1, getName, eraseScreen1 } from "./modules/screen1";
+import { createScreen1, eraseScreen1, getName } from "./modules/screen1";
 import { screen2Logic } from "./modules/screen2";
-import { Ship } from "./modules/ship";
+import { screen3Logic } from "./modules/screen3";
 
 const body = document.querySelector("body");
-let name, player, ai, screen2, playerCells, aiCells, messageScreen3;
-//Screen 1
-
+let name;
 createScreen1();
 const submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", (e) => {
-  const playerName = getName();
+  name = getName();
   e.preventDefault();
-  name = playerName;
   setTimeout(() => {
     eraseScreen1();
-    player = Player(name);
-    ai = Computer();
   }, 800);
 });
-
-//Screen 2
-const shipsInfo = [
-  ["carrier", 5],
-  ["battleship", 4],
-  ["destroyer", 3],
-  ["submarine", 3],
-  ["patrolBoat", 2],
-];
-let currentShip;
-let playerBoard, aiBoard;
-//Create screen2 when screen1 gets deleted via mutation observer
 const screen1Observer = new MutationObserver(() => {
-  screen2Logic(playerBoard, aiBoard, shipsInfo, currentShip);
-  screen2 = document.querySelector(".screen2");
+  console.log(name);
+  const [playerBoard, aiBoard] = screen2Logic(name);
   const screen2Observer = new MutationObserver(() => {
-    console.log("hello");
+    console.log(name);
+    screen3Logic(playerBoard, aiBoard, name);
+    screen2Observer.disconnect();
   });
   screen2Observer.observe(body, { attributes: true });
   screen1Observer.disconnect();
